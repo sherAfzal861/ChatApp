cb1691bb02cc1e407e887e7d7bb224ce2d1dd219
SherAfzal861 <ksherafzal861@gmail.com>
2023-08-17 12:38:12 +0500

    made responsive

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template,  request, redirect, url_for, flash
-from flask_socketio import SocketIO, emit
+from flask import Flask
+from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
